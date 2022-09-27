1694b00088f8e3ca1c5e34545284b19e55317547
Sarah Moradi <moradisarah2001@gamil.com>
2022-09-27 10:19:09 +0330

    completed cards to show products and stablished design

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,25 +3,34 @@ import * as data from '../data'
 import Layout from '../Layout/Layout'
 
 const HomePage = () => {
+  const addToCartHandler = (product) => {
+    console.log(product)
+  }
   return (
     <Layout>
-      <main>
-        <section>
-          <span>
-            {data.products.map((product) => {
-              return (
-                <section>
-                  <div>
-                    <img src={product.image} alt={product.name} />
-                  </div>
-                  <div>
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
-                  </div>
-                </section>
-              )
-            })}
-          </span>
+      <main className='container'>
+        <section className='productList'>
+          {data.products.map((product) => {
+            return (
+              <section className='product'>
+                <div>
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    width={230}
+                    height={150}
+                  />
+                </div>
+                <div className='productDesc'>
+                  <p>{product.name}</p>
+                  <p>${product.price}</p>
+                  <button onClick={() => addToCartHandler(product)}>
+                    Add to cart
+                  </button>
+                </div>
+              </section>
+            )
+          })}
         </section>
       </main>
     </Layout>
